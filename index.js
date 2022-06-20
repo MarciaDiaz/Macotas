7011a4167a448856551203ac0eaa752255e38120
marcia diaz <marciadiazz92@gmail.com>
2022-06-20 15:42:44 -0300

    cambie de consola a alert

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ let perro = prompt("ingrese el nombre de su perro");
 let peso = parseInt(prompt("ingrese el peso de su perro"));
 let su_dosis = calcular_dosis(peso);
 
-console.log("nombre: ", perro);
-console.log ("el peso de su perro es ", peso , "kg");
-console.log("La dosis a tomar cada 12hs es: ",su_dosis, "ml")
+alert ("El nombre de su perro es: " + perro + "\n" + "El peso de su perro es: " + peso + "kg\n" + "La dosis a tomar cada 12hs es: " + su_dosis + "ml")
 
 }
+
