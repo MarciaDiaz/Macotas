f47f7ebc080e0db9682a3cbc9a098602fc76b91a
marcia diaz <marciadiazz92@gmail.com>
2022-06-20 14:08:59 -0300

    agregue el ciclo

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 
+
 function calcular_dosis (peso){
 // retorna dosis
 
-    let dosis = peso * 0.25;
-    return dosis
+let dosis = peso * 0.25;
+return dosis
 }
 
+for (let i=1 ; i<5 ; i++){
 
-console.log("Bienvenido a la veterinaria");
-let perro = prompt("Ingrese el nombre de su perro");
-let peso = parseInt(prompt("Ingrese el peso de su perro"));
+ 
+alert ( "Hola  su turno es el numero: " + i)
+
+console.log("bienvenido a la veterinaria");
+let perro = prompt("ingrese el nombre de su perro");
+let peso = parseInt(prompt("ingrese el peso de su perro"));
 let su_dosis = calcular_dosis(peso);
 
-console.log("Nombre: ", perro);
-console.log ("El peso de su perro es ", peso , "kg");
+console.log("nombre: ", perro);
+console.log ("el peso de su perro es ", peso , "kg");
 console.log("La dosis a tomar cada 12hs es: ",su_dosis, "ml")
 
-
+}
