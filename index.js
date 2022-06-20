1419dcf60564488b83cff2988fe9a89f5cf956cf
marcia diaz <marciadiazz92@gmail.com>
2022-06-20 13:41:08 -0300

    nueva tarea

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 
-for (let i=1 ; i<6 ; i++){
+function calcular_dosis (peso){
+// retorna dosis
 
-    alert ("Bienvenido a veterinaria online")
-
-    let nombre = prompt ("Ingrese su nombre");
- 
-alert ( "Hola " + nombre + " su turno es el numero: " + i)
-
-let mascota = prompt ("Ingrese mascota" + "\n" + "1- Gato" + "\n" +  "2-Perro" + "\n" + "3-Ninguna de las anteriores");
-
-
-
-    if ( mascota == 1){
-
-   alert ("Su mascota sera atendido por especialistas en michis")
-    }
-    else if (mascota == 2){
-    alert ("Su mascota sera atendida por especialista en perritos")
-    }
-    else if (mascota == 3) {
-    alert ("Sera derivado con una veterianaria de exoticos")
-    }
-    else{
-    alert ("la opcion no es valida intentelo nuevamente")
-    break
-    }
+    let dosis = peso * 0.25;
+    return dosis
 }
+
+
+console.log("Bienvenido a la veterinaria");
+let perro = prompt("Ingrese el nombre de su perro");
+let peso = parseInt(prompt("Ingrese el peso de su perro"));
+let su_dosis = calcular_dosis(peso);
+
+console.log("Nombre: ", perro);
+console.log ("El peso de su perro es ", peso , "kg");
+console.log("La dosis a tomar cada 12hs es: ",su_dosis, "ml")
+
+
